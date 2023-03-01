d04f25a291aa66ec2fccbb95aa49f799edaab60e
danutdotrar <morarasu.danut@gmail.com>
2023-03-01 18:42:24 +0200

    Add Gameboard Module Function

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-// Tic Tac Toe
+const row = document.querySelector("#row");
 
-// Steps:
-// am nevoie de un gameboard - defineste gameboard - un grid de 3x3
-//
+// Store the gameboard as an array inside the Gameboard object
+// Use a module with IIFE to create a single gameboard
+const Gameboard = (() => {
+    const table = ["", "", "", "", "", "", "", "", ""];
+
+    const createTable = () => {
+        for (let i = 0; i < table.length; i++) {
+            const cell = document.createElement("div");
+            cell.className = "cell";
+            cell.innerHTML = table[i];
+            row.appendChild(cell);
+        }
+    };
+
+    return { createTable };
+})();
+
+// Player Factory Object
+const PlayerFactory = (name, mark) => {
+    return { name, mark };
+};
+
+// Players
+const player1 = PlayerFactory("Player 1", "X");
+const player2 = PlayerFactory("Player 2", "O");
+
+// Create table
+Gameboard.createTable();
